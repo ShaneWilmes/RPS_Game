800dba19503f80613f05101aae647a92d054b7c9
ShaneWilmes <shanewilmes@yahoo.com>
2022-04-19 15:26:12 -0500

    changes

--- assets/script.js
+++ assets/script.js
@@ -18,7 +18,43 @@ var playerChoice = window.prompt("Enter R, P, or P");
 //  Converting to uppercase text input
 playerChoice = playerChoice.toUpperCase(); 
 
-//  Getting computer's choice
+//  Randomizing and getting the computer's choice
+var randomChoice = Math.floor(Math.random() * options.length);
+var computerChoice = options[randomChoice];
+alert("The computer chose " + computerChoice);
+
+//  Verifying the wins, losses and ties
+if (playerChoice === computerChoice) {
+    ties++
+    alert("It's a tie!");
+
+} else if (
+    (playerChoice === 'R' && computerChoice === 'S') ||
+    (playerChoice === 'P' && computerChoice === 'R') ||
+    (playerChoice === 'S' && computerChoice === 'P')
+
+) {
+    wins++;
+    alert("You won!");
+    
+} else {
+    losses++;
+    alert("You lost!");
+
+}
+
+//  Displaying the stats and asking to play again
+
+alert("Stats: " + wins + " Losses: " + losses + "Ties: " + ties);
+
+//  Asking the user to play again
+var startOver = confirm("Play again?");
+
+if (startOver) {
+    playGame();
+}
 
 
 }
+
+playGame()  //  Calling function to start the game
