cb78b8671b00c485875b7409dc213e7265db782e
ShaneWilmes <shanewilmes@yahoo.com>
2022-04-19 15:11:04 -0500

    first commit

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+//  ROCK**PAPER**SCISSORS GAME
+
+//  Setting variables of wins and losses
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
+//  Setting an arry for the choices
+var options = ['R', 'P', 'S'];
+
+
+//  Function Play Game to execute the game
+function playGame () {
+
+//  Getting player's choice    
+var playerChoice = window.prompt("Enter R, P, or P"); 
+
+//  Converting to uppercase text input
+playerChoice = playerChoice.toUpperCase(); 
+
+//  Getting computer's choice
+
+
+}
